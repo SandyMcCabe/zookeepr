d14962802d1c071ba5f2fc4de019ad83eed0355d
Sandy McCabe <sandytech13@gmail.com>
2021-10-31 20:17:54 -0500

    Heroku and some additional api calls

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ const app = express();
 
 const { animals } = require('./data/animals');
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
     res.json(animals);
   });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
